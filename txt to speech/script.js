970ba434ff9cd5d3dc59ce7ca1f42a31b12d9bf6
Irsa911 <140706602+Irsa911@users.noreply.github.com>
2023-12-23 10:23:27 +0500

    Update script.js

--- txt to speech/script.js
+++ txt to speech/script.js
@@ -1,31 +1,3 @@
-// let speech = new SpeechSynthesisUtterance();
-
-// document.querySelector("button").addEventListener("click",()=>{
-//     document.text = document.querySelector("textarea").value;
-//     window.speechSynthesis.speak(speech);
-// })
-
-
-
-// let speech = new SpeechSynthesisUtterance();
-
-// let voice =[];
-// let voiceSelect =document.querySelector("select");
-// window.speechSynthesis.onvoiceschanged=()=>{
-//     voices =window.speechSynthesis.getVoices();
-//     speech.voice = voices[0];
-//     voices.forEach((voice,i)=>(voiceSelect.options[i])=new Option((voice.name,i)))
-        
-   
-// };
-
-// document.querySelector("button").addEventListener("click", () => {
-//     let text = document.querySelector("textarea").value;
-//     speech.text = text;
-//     window.speechSynthesis.speak(speech);
-// });
-
-
 let speech = new SpeechSynthesisUtterance();
 let voiceSelect = document.querySelector("select");
 
